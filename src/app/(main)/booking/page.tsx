a7c87eccf2ab11de911983632dcabe317d711c4f
thanaboo <zatwotinz@gmail.com>
2023-11-28 17:16:47 +0700

    add-shadcn & format-page

---
+++ src/app/(main)/booking/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BookingPage() {
+  return <div>Bookingpage</div>;
+}
+
+export default BookingPage;
---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,6 @@
+import PageWrapper from "@/components/wrapper/page-wrapper";
+import Image from "next/image";
+
+export default function Home() {
+  return <PageWrapper>TODO:add some content</PageWrapper>;
+}
