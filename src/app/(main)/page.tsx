20112b079de348169f1f5a890d697e0203b7931b
thanaboo <zatwotinz@gmail.com>
2023-11-28 18:00:54 +0700

    edit home-page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,5 +2,19 @@ import PageWrapper from "@/components/wrapper/page-wrapper";
 import Image from "next/image";
 
 export default function Home() {
-  return <PageWrapper>TODO:add some content</PageWrapper>;
+  return (
+    <PageWrapper>
+      <div className="mt-24 mb-24">
+        <h1 className="text-6xl text-black">Singla</h1>
+        <p className="flex justify-end text-1xl">TODO:ข้อมูลโรงแรม</p>
+        <Image
+          src={"/assets/bg.jpg"}
+          alt="404"
+          width={1400}
+          height={500}
+          className="object-cover w-full h-96"
+        />
+      </div>
+    </PageWrapper>
+  );
 }
