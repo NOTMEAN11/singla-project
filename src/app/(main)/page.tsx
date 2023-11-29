b16afaeb610083aa5aab1b8033d851393ea48349
thanaboo <zatwotinz@gmail.com>
2023-11-29 17:26:31 +0700

    update-page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,20 +1,14 @@
+import AboutSection from "@/components/section/aboutsection";
+import HeroSection from "@/components/section/herosection";
+import { Button } from "@/components/ui/button";
 import PageWrapper from "@/components/wrapper/page-wrapper";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <PageWrapper>
-      <div className="mt-24 mb-24">
-        <h1 className="text-6xl text-black">Singla</h1>
-        <p className="flex justify-end text-1xl">TODO:ข้อมูลโรงแรม</p>
-        <Image
-          src={"/assets/bg.jpg"}
-          alt="404"
-          width={1400}
-          height={500}
-          className="object-cover w-full h-96"
-        />
-      </div>
+      <HeroSection />
+      <AboutSection />
     </PageWrapper>
   );
 }
