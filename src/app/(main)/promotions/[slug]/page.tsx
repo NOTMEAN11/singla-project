b652c7dc1f05a8ea33a0a55f256808840ceffd2a
thanaboo <zatwotinz@gmail.com>
2023-12-11 19:46:32 +0700

    add-promotions & slug-page

---
+++ src/app/(main)/promotions/[slug]/page.tsx
@@ -0,0 +1,140 @@
+import ContainerWrapper from "@/components/wrapper/container-wrapper";
+import PageWrapper from "@/components/wrapper/page-wrapper";
+import React from "react";
+import Image from "next/image";
+import PromotionsCard from "@/components/promotions-card";
+import PageHeader from "@/components/pageheader/pageheader";
+import { notFound } from "next/navigation";
+
+type PromotionType = {
+  title: string;
+  content: string;
+  image: string;
+  slug: string;
+};
+//TODO: เปลี่ยน slug
+const promotions: PromotionType[] = [
+  {
+    title: "นาทีสุดท้าย",
+    content:
+      "ราคาดีที่สุดสำหรับการพักผ่อนที่มหาครเมืองสงขลา ที่พักในฝันสำหรับวันหยุดในเมือง",
+    image: "/assets/imgpromotions/promotions1.jpg",
+    slug: "last-minute",
+  },
+  {
+    title: "แพ็กเกจ",
+    content:
+      "คลื่นแห่งการเฉลิมฉลอง: การเดินทางที่เต็มอิ่ม พบประสบการณ์สุดพิเศษกับแพ็คเกจซิกเนเจอร์ ในโอกาสครบรอบ",
+    image: "/assets/imgpromotions/promotions2.jpg",
+    slug: "faded-stay",
+  },
+  {
+    title: "ห้องพักและอาหารเช้า",
+    content: "SINGLA ช่วงเวลาดีดีแห่งความสุขตรงนี้ของคุณ",
+    image: "/assets/imgpromotions/promotions3.jpg",
+    slug: "faded-stay",
+  },
+  {
+    title: "แพ็กเกจ",
+    content: "ยิ่งพักนาน ยิ่งคุ้ม พลาดไม่ได้กับข้อเสนอยิ่งพักนาน ยิ่งคุ้ม",
+    image: "/assets/imgpromotions/promotions4.jpg",
+    slug: "faded-stay",
+  },
+  {
+    title: "ข้อเสนอห้องพัก",
+    content:
+      "พักก่อน จ่ายทีหลัง ผ่อนชำระแบบไม่มีดอกเบี้ย สำหรับผู้ถือบัตรที่ร่วมรายการ",
+    image: "/assets/imgpromotions/promotions5.jpg",
+    slug: "faded-stay",
+  },
+];
+
+function PromotionPage({ params }: { params: { slug: string } }) {
+  const promotion = promotions.find(
+    (promotion) => promotion.slug === params.slug
+  );
+
+  if (!promotion) {
+    return notFound();
+  }
+  return (
+    <PageWrapper className="mt-20 mb-8">
+      <PageHeader
+        title={promotion.title}
+        disableTitle
+        className="container"
+        breadcrumb={[
+          { title: "โปรโมชั่น", path: "/promotions" },
+          { title: promotion.title, path: `/promotions/${promotion.slug}` },
+        ]}
+      />
+      <ContainerWrapper>
+        <div>
+          <h1 className="text-3xl font-bold">{promotion.title}</h1>
+          <p>{promotion.content}</p>
+          <ul className="ml-5 list-disc">
+            <li>Lorem, ipsum.</li>
+            <li>Lorem, ipsum.</li>
+            <li>Lorem, ipsum.</li>
+          </ul>
+          <p>
+            <span className="font-bold">
+              ระยะการจอง : 10 ธันวาคม, 2020 - 31 ธันวาคม 2023
+            </span>
+          </p>
+          <p>
+            <span className="font-bold">คูปองลดส่วน </span>: SEPELGQWXL23
+          </p>
+          <div>
+            <h2 className="font-bold">ข้อกำหนดและเงื่อนไข</h2>
+            <p>
+              โปรดชำระเงินเต็มจำนวนที่โรงแรม
+              การจองห้องพักนี้ไม่สามารถบอกยกเลิกหรือปรับเปลี่ยนได้
+              ในกรณีที่ไม่มาเข้าพัก จะคิดค่าปรับเป็นเวลา 1 คืน
+              ราคาตามโปรแกรมเฟล็กซี่ เบสต์ อะเวเลเบิ้ล (ไม่รวมค่าบริการและภาษี)
+            </p>
+          </div>
+          <div>
+            <h2 className="font-bold text-lg">ข้อกำหนดทั่วไปและเงื่อนไข</h2>
+            <ul className="list-disc ml-5">
+              <li>
+                ราคานี้เป็นราคาต่อห้องต่อคืนและยังไม่รวมค่าบริการ 10%
+                และภาษีมูลค่าเพิ่ม 7%
+              </li>
+              <li>
+                ราคานี้เป็นราคาสำหรับการจองส่วนบุคคลเท่านั้นและขึ้นอยู่กับห้องพักที่ว่าง
+                ณ ขณะทำการจอง
+              </li>
+              <li>ราคานี้ไม่สามารถใช้ร่วมกับโปรโมชั่นอื่นๆ ได้</li>
+              <li>ราคานี้อาจมีการเปลี่ยนแปลงได้ตามความพร้อมของโรงแรม</li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="font-bold text-lg">
+              นโยบายการชำระเงิน การยกเลิก การแก้ไข และการไม่เข้าพัก:
+            </h2>
+            <ul className="list-disc ml-5">
+              <li>
+                ทุกการจองต้องใช้หมายเลขบัตรเครดิตที่ใช้งานได้ตลอดการเข้าพักเป็นการรับประกันการจอง
+              </li>
+              <li>
+                หากการจองห้องพักถูกยกเลิกก่อนการเข้าพักก่อนวันเข้าพักน้อย 24
+                ชั่วโมงจะมีการเรียกเก็บค่าห้อง 1 คืน
+              </li>
+              <li>
+                ในกรณีที่ไม่มาเข้าพักหรือเข้าพักน้อยกว่าหรือเร็วกว่ากำหนด
+                จะมีการเรียกเก็บค่าห้อง 1 คืน
+              </li>
+              <li>
+                การแก้ไขการสำรองห้องพักสามารถทำได้โดยขึ้นอยู่กับห้องพักที่ว่าง ณ
+                ขณะทำการจอง
+              </li>
+            </ul>
+          </div>
+        </div>
+      </ContainerWrapper>
+    </PageWrapper>
+  );
+}
+
+export default PromotionPage;
--- src/app/(main)/promotions/page.tsx
+++ src/app/(main)/promotions/page.tsx
@@ -2,92 +2,65 @@ import ContainerWrapper from "@/components/wrapper/container-wrapper";
 import PageWrapper from "@/components/wrapper/page-wrapper";
 import React from "react";
 import Image from "next/image";
+import PromotionsCard from "@/components/promotions-card";
+import PageHeader from "@/components/pageheader/pageheader";
+
+type PromotionType = {
+  title: string;
+  content: string;
+  image: string;
+  slug: string;
+};
+
+const promotions: PromotionType[] = [
+  {
+    title: "นาทีสุดท้าย",
+    content:
+      "ราคาดีที่สุดสำหรับการพักผ่อนที่มหาครเมืองกรุงเทพ ที่พักในฝันสำหรับวันหยุดในเมือง",
+    image: "/assets/imgpromotions/promotions1.jpg",
+    slug: "last-minute",
+  },
+  {
+    title: "แพ็กเกจ",
+    content:
+      "คลื่นแห่งการเฉลิมฉลอง: การเดินทางที่เต็มอิ่ม พบประสบการณ์สุดพิเศษกับแพ็คเกจซิกเนเจอร์ ในโอกาสครบรอบ",
+    image: "/assets/imgpromotions/promotions2.jpg",
+    slug: "faded-stay",
+  },
+  {
+    title: "ห้องพักและอาหารเช้า",
+    content: "SINGLA ช่วงเวลาดีดีแห่งความสุขตรงนี้ของคุณ",
+    image: "/assets/imgpromotions/promotions3.jpg",
+    slug: "faded-stay",
+  },
+  {
+    title: "แพ็กเกจ",
+    content: "ยิ่งพักนาน ยิ่งคุ้ม พลาดไม่ได้กับข้อเสนอยิ่งพักนาน ยิ่งคุ้ม",
+    image: "/assets/imgpromotions/promotions4.jpg",
+    slug: "faded-stay",
+  },
+  {
+    title: "ข้อเสนอห้องพัก",
+    content:
+      "พักก่อน จ่ายทีหลัง ผ่อนชำระแบบไม่มีดอกเบี้ย สำหรับผู้ถือบัตรที่ร่วมรายการ",
+    image: "/assets/imgpromotions/promotions5.jpg",
+    slug: "faded-stay",
+  },
+];
 
 function PromotionsPage() {
   return (
-    <PageWrapper className="mt-20">
-      <h1 className="flex justify-center items-center uppercase font-bold my-24">
-        promotions singla
-      </h1>
+    <PageWrapper className="mt-20 mb-4">
+      <PageHeader
+        title="โปรโมชั่นทั้งหมด"
+        className="container"
+        breadcrumb={[{ title: "โปรโมชั่น", path: "/promotions" }]}
+      />
       <ContainerWrapper className="grid justify-center items-center">
-        <div className="grid-cols-3 grid">
-          <div className="relative">
-            <p className="absolute text-white font-bold text-md">นาทีสุดท้าย</p>
-            <Image
-              src="/assets/imgpromotions/promotions1.jpg"
-              alt="400x200"
-              width={400}
-              height={200}
-            />
-            <p className="absolute bottom-1 text-white font-bold text-md">
-              ราคาดีที่สุดสำหรับการพักผ่อนที่มหาครเมืองกรุงเทพ
-              ที่พักในฝันสำหรับวันหยุดในเมือง
-            </p>
-          </div>
-          <div className="relative">
-            <p className="absolute text-white font-bold text-md">แพ็กเกจ</p>
-            <Image
-              src="/assets/imgpromotions/promotions2.jpg"
-              alt="400x200"
-              width={400}
-              height={200}
-            />
-            <p className="absolute bottom-1 text-white font-bold">
-              คลื่นแห่งการเฉลิมฉลอง: การเดินทางที่เต็มอิ่ม
-              <p></p>
-              <span>
-                พบประสบการณ์สุดพิเศษกับแพ็คเกจซิกเนเจอร์ ในโอกาสครบรอบ
-              </span>
-            </p>
-          </div>
-          <div className="relative">
-            <p className="absolute text-white font-bold text-md">
-              ห้องพักและอาหารเช้า
-            </p>
-            <Image
-              src="/assets/imgpromotions/promotions3.jpg"
-              alt="400x200"
-              width={400}
-              height={200}
-            />
-            <p className="absolute bottom-1 text-white font-bold">
-              SINGLA
-              <p></p>
-              <span>ช่วงเวลาดีดีแห่งความสุขตรงนี้ของคุณ</span>
-            </p>
-          </div>
-          <div className="relative">
-            <p className="absolute text-white font-bold text-md">แพ็กเกจ</p>
-            <Image
-              src="/assets/imgpromotions/promotions4.jpg"
-              alt="400x200"
-              width={400}
-              height={200}
-            />
-            <p className="absolute text-white font-bold bottom-1">
-              ยิ่งพักนาน ยิ่งคุ้ม
-              <p></p>
-              <span>พลาดไม่ได้กับข้อเสนอยิ่งพักนาน ยิ่งคุ้ม</span>
-            </p>
-          </div>
-          <div className="relative">
-            <p className="absolute text-white font-bold text-md">
-              ข้อเสนอห้องพัก
-            </p>
-            <Image
-              src="/assets/imgpromotions/promotions5.jpg"
-              alt="400x200"
-              width={400}
-              height={200}
-            />
-            <p className="absolute text-white font-bold bottom-1">
-              พักก่อน จ่ายทีหลัง
-              <p></p>
-              <span>
-                ผ่อนชำระแบบไม่มีดอกเบี้ย สำหรับผู้ถือบัตรที่ร่วมรายการ
-              </span>
-            </p>
-          </div>
+        <div className="grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid gap-2 mb-4">
+          {promotions.map((promotion) => (
+            <PromotionsCard {...promotion} key={promotion.slug} />
+          ))}
         </div>
       </ContainerWrapper>
     </PageWrapper>
