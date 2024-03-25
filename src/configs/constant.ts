08aa120902f4162114b33615b27e07f2be88e697
NOTMEAN11 <jirateep.loyma02@gmail.com>
2024-03-26 02:37:27 +0700

    remove unuse var

--- src/configs/constant.ts
+++ src/configs/constant.ts
@@ -1,5 +1,3 @@
-import { RoomType } from "@/types/booking";
-
 const menu = [
   { name: "โปรโมชั่น", Link: "/promotions" },
   { name: "ติดต่อเรา", Link: "/contact-us" },
@@ -11,39 +9,4 @@ const room = [
   { name: "ทั้งหมด", Link: "/rooms" },
 ];
 
-const roomtype: RoomType[] = [
-  {
-    id: "1",
-    name: "SUPERIOR ROOM",
-    price: 1500,
-    desc: "ห้องพักแบบสุดคุ้ม พร้อมอุปกรณ์ครบครัน พักได้สูงสุด 2 ท่าน",
-    image: ["/assets/rooms/room1.2.jpg"],
-    slug: "superior-room",
-  },
-  {
-    id: "2",
-    name: "DELUXE ROOM",
-    price: 2000,
-    desc: "ห้องพักแบบสุดหรู พร้อมอุปกรณ์ครบครัน พักได้สูงสุด 2 ท่าน",
-    image: ["/assets/rooms/room2.2.jpg"],
-    slug: "deluxe-room",
-  },
-  {
-    id: "3",
-    name: "FAMILY ROOM",
-    price: 3000,
-    desc: "ห้องพักแบบสุดคุ้ม พร้อมอุปกรณ์ครบครัน พักได้สูงสุด 4 ท่าน",
-    image: ["/assets/rooms/room3.jpg"],
-    slug: "family-room",
-  },
-  {
-    id: "4",
-    name: "POOL SUITE",
-    price: 4000,
-    desc: "ห้องพักแบบสุดหรู พร้อมอุปกรณ์ครบครัน พักได้สูงสุด 2 ท่าน",
-    image: ["/assets/rooms/room4.2.jpg"],
-    slug: "pool-suite",
-  },
-];
-
-export { menu, room, roomtype };
+export { menu, room };
